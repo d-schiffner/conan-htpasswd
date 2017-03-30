3a1b8fd215f5c345a13526d38ffd505dfe4f6e7f
Daniel Schiffner <dschiffner@gdv.cs.uni-frankfurt.de>
2017-03-30 12:26:28 +0200

    Added search for config file

--- htpasswd_authenticator.py
+++ htpasswd_authenticator.py
@@ -2,9 +2,13 @@
 A plugin for conan htpasswd authentication
 
 TODO:
-Make location of htpasswd file configurable
+* Make location of htpasswd file configurable
 """
+
+import os
+from ConfigParser import ConfigParser
 from passlib.apache import HtpasswdFile
+from conans.path import conan_expand_user
 
 def get_class():
     """ plugin entry point """
@@ -17,7 +21,18 @@ class HtpasswdAuthenticator(object):
     htpasswd is the location of the htpasswd to use
     """
     def __init__(self):
-        self.htpasswd = HtpasswdFile(".htpasswd")
+        #try to locate the server config and load it
+        server_folder = os.path.join(conan_expand_user("~"), '.conan_server')
+        server_conf = os.path.join(server_folder, "server.conf")
+        htpasswd_location = os.path.join(server_folder, "plugins", "authentication", ".htpasswd")
+        if os.path.exists(server_conf):
+            conf = ConfigParser()
+            conf.read(server_conf)
+            new_loc = conf.get("server", "htpasswd_file")
+            if os.path.exists(new_loc):
+                htpasswd_location = new_loc
+
+        self.htpasswd = HtpasswdFile(htpasswd_location)
 
     def valid_user(self, username, plain_password):
         """
